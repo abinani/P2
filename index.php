8b547b5e7905c1951cca022dc5055752e2965f7e
abinani <abinani@fas.harvard.edu>
2014-07-07 16:40:04 -0400

    Learn using bootstrap. v2

--- index.php
+++ index.php
@@ -1,37 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <title>XKCD password generator</title>
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css" rel="stylesheet">
-</head>
-<body role="document" style="padding-top: 20px;">
-  <div class="row"></div>
-  <div class="container" role="main">
-  <div class="jumbotron">
-      <h1>XKCD Password Generator!</h1>
-  </div>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css" rel="stylesheet">
+  </head>
+  <body role="document" style="padding-top: 20px;">
+    <div class="container" role="main">
+      <div class="jumbotron">
+        <h1>XKCD Password Generator!</h1>
+      </div>
 
-<form role="form">
-  <div class="form-group form-inline">
-    <h3>
-    <span class="label label-default" for="numberOfWords">Number of Words</span>
-    <select class="form-control">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-    </select>
-    </h3>
-  </div>
-  <button type="submit" class="btn btn-default">Get Me!</button>
-</form>  
-</div>
-</body>
+      <form role="form">
+        <div class="form-group form-inline">
+          <h3>
+            <span class="label label-default" for="numberOfWords">Number of Words</span>
+            <select class="form-control">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select>
+          </h3>
+        </div>
+        <button type="submit" class="btn btn-default">Get Me!</button>
+      </form>  
+    </div>
+  </body>
 </html>
 
