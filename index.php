6692158a1ea4471053653d0e125420aec60fac52
abinani <abinani@fas.harvard.edu>
2014-07-07 16:14:00 -0400

    Learn using bootstrap.

--- index.php
+++ index.php
@@ -2,10 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>XKCD password generator</title>
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css" rel="stylesheet">
 </head>
-<body>
-    
+<body role="document" style="padding-top: 20px;">
+  <div class="row"></div>
+  <div class="container" role="main">
+  <div class="jumbotron">
+      <h1>XKCD Password Generator!</h1>
+  </div>
+
+<form role="form">
+  <div class="form-group form-inline">
+    <h3>
+    <span class="label label-default" for="numberOfWords">Number of Words</span>
+    <select class="form-control">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+      <option>9</option>
+    </select>
+    </h3>
+  </div>
+  <button type="submit" class="btn btn-default">Get Me!</button>
+</form>  
+</div>
 </body>
 </html>
 
